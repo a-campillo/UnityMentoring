c7deaa40b3cc4b6890e6f1df228bcb72ca78d8f7
Andres Perez Campillo <74317290+a-campillo@users.noreply.github.com>
2023-01-31 17:15:38 -0500

    Added coin model, rotation, level barriers

--- Assets/SurvivalProj/Scripts/CharacterBehaviour.cs
+++ Assets/SurvivalProj/Scripts/CharacterBehaviour.cs
@@ -19,11 +19,17 @@ namespace SurvivalProj.Behaviours
         private float xVal = 0F;
         private float yVal = 0F;
 
+        private Vector2 rotValue;
+        private float currentMousePosition = 0F;
+        private bool facingForward = true;
+
         private bool isRunning = false;
 
         // Start is called before the first frame update
         private void Start()
         {
+            Cursor.lockState = CursorLockMode.Confined;
+
             character = new Character("Hero", 10);
             weapon = new Weapon("PeyeSword");
         }
@@ -31,47 +37,40 @@ namespace SurvivalProj.Behaviours
         // Update is called once per frame
         private void Update()
         {
+            ProcessMovement();
+
+            ProcessRotation();
+        }
+
+        private void ProcessMovement()
+        {
             xVal = Input.GetAxis("Horizontal");
             yVal = Input.GetAxis("Vertical");
-
-            if (xVal != 0F)
-            {
-                //isRunning = Input.GetAxis("Fire2") > 0;
-
-                //transform.Translate(Vector3.right * (isRunning ? runSpeed : moveSpeed) * xVal * Time.deltaTime);
-
-                transform.Rotate(Vector3.up * xVal * rotationSpeed * Time.deltaTime);
-
-            }
 
             if (yVal != 0F)
             {
                 isRunning = Input.GetAxis("Fire2") > 0;
 
-                //transform.Translate(Vector3.forward * (isRunning ? runSpeed : moveSpeed) * yVal * Time.deltaTime);
-
                 if (isRunning)
                 {
                     transform.Translate(Vector3.forward * runSpeed * yVal * Time.deltaTime);
-
                 }
                 else
                 {
                     transform.Translate(Vector3.forward * moveSpeed * yVal * Time.deltaTime);
                 }
             }
+        }
 
-            //transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime);
-
-            //if (Input.GetKey(KeyCode.Q))
-            //{
-            //    transform.Rotate(Vector3.up * -rotationSpeed * Time.deltaTime);
-            //}
-            
-            //else if (Input.GetKey(KeyCode.E))
-            //{
-            //    transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-            //}
+        private void ProcessRotation()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                transform.LookAt(hit.point); // Look at the point
+                transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
+            }
         }
     }
 }
